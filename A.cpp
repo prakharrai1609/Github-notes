d1cfae239c5ee1cff9f151b460623e5934628f6a
Prakhar <rai.prakhar1609@gmail.com>
2022-08-07 14:08:10 +0530

    first commit

---
+++ A.cpp
@@ -0,0 +1,45 @@
+/*    Author : Prakhar Rai    */
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long          ll;
+typedef long double        ld;
+
+#define FIO         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ln(s)	    ll(s.length())
+#define sz(a)	    ll(a.size())
+#define pb          push_back
+#define fs          first
+#define sc          second
+#define vcll        vector<ll>
+#define all(x)      x.begin(),x.end()
+#define endl        "\n"
+#define vc          vector
+#define FOR(i,a,b)  for(ll i = a; i < b; i++)
+#define pika(x)     cerr << "Line #" << (__LINE__) << " : " << #x <<" --> " << x; cerr << endl;
+#define rprint(x)   return void(cout << x);
+#define yes         "YES"
+#define no          "NO"
+
+const int mod = 1e9 + 7;
+const int MOD = 998244353;
+const int N = 2e5 + 5;
+
+
+
+void test_case() {
+    
+}
+
+
+
+int main() {
+    FIO;
+    int _tests;
+    _tests = 1;
+    // cin >> _tests;
+    for (int i = 1; i <= _tests; i++) {
+        // cout << "Case #" << i << ": ";
+        test_case();
+    }
+}
